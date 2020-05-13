13b4f62e03da9880183881117f6f2f1498aac3af
alifrizky <alifrzky78@gmail.com>
2020-05-13 13:40:24 +0700

    first commit, pemulaan untuk membuat icon dan desain layout

---
+++ app/src/main/java/com/training/alif/geeksfarm/e_pohonkarrier/MainActivity.java
@@ -0,0 +1,14 @@
+package com.training.alif.geeksfarm.e_pohonkarrier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
